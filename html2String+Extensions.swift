3b934cf9ed68d2053f53c3bcaad1ccdf827753ed
sspux <eineadressevon@googlemail.com>
2015-08-06 18:47:23 +0200

    Update html2String+Extensions.swift

--- html2String+Extensions.swift
+++ html2String+Extensions.swift
@@ -1,9 +1,6 @@
 //
 //  html2String+Extensions.swift
 //
-//  Created by HeikoG on 06.08.15.
-//  Copyright © 2015 HeikoG. All rights reserved.
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
